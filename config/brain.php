bda5e92f1ab5ce94194fe6d008a61e8becc41d55
Rafael Lunardelli <rafael@lunardelli.me>
2025-03-18 16:13:46 -0600

    feat(config): update config structure for brain processes
    Replaced 'process' context with 'root_dir' in the config file. This establishes a main directory for all processes, tasks, and queries under 'Brain'. Enhances clarity and setup flexibility for application structure.

---
+++ config/brain.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Root dir
+    |--------------------------------------------------------------------------
+    |
+    | This value set's the main directory where we will create all the processes
+    | tasks, and queries.
+    |
+    */
+    'root_dir' => app_path('Brain'),
+];

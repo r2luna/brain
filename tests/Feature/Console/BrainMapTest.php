95756a91aff09783a8710b1b000261a0774ef946
Rafael Lunardelli <rafael@lunardelli.me>
2025-03-19 10:09:22 -0600

    test: add tests for getReflectionClass method in BrainMap

--- tests/Feature/Console/BrainMapTest.php
+++ tests/Feature/Console/BrainMapTest.php
@@ -2,6 +2,28 @@
 
 declare(strict_types=1);
 
+beforeEach(function () {
+    $this->object = new Brain\Console\BrainMap;
+    $this->reflection = new ReflectionClass(get_class($this->object));
+});
+
+describe('getReflectionClass testsuite', function () {
+    it('should create a reflection class from a given string', function () {
+        $method = $this->reflection->getMethod('getReflectionClass');
+        $path = '\Tests\Feature\Fixtures\QueuedTask';
+        $output = $method->invokeArgs($this->object, [$path]);
+
+        expect($output)->toBeInstanceOf(ReflectionClass::class);
+    });
+
+    it('should create a reflection class from a SplFileInfo', function () {
+        $method = $this->reflection->getMethod('getReflectionClass');
+        $path = new SplFileInfo(__DIR__.'/../Fixtures/QueuedTask.php');
+        $output = $method->invokeArgs($this->object, [$path]);
+        expect($output)->toBeInstanceOf(ReflectionClass::class);
+    });
+});
+
 describe('getClassFullNameFromFile testsuite', function () {
 
     it('should get the full name from a file', function () {
@@ -20,13 +42,10 @@ PHP;
         $filePath = tempnam(sys_get_temp_dir(), 'TestFile');
         file_put_contents($filePath, $content);
 
-        $object = new Brain\Console\BrainMap;
-
-        $reflection = new ReflectionClass(get_class($object));
-        $method = $reflection->getMethod('getClassFullNameFromFile');
+        $method = $this->reflection->getMethod('getClassFullNameFromFile');
         $method->setAccessible(true);
 
-        $output = $method->invokeArgs($object, [$filePath]);
+        $output = $method->invokeArgs($this->object, [$filePath]);
 
         unlink($filePath);
 

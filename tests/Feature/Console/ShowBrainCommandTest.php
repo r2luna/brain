5f69ac294a22dafa8321fb615cdc7c9b40b6d485
WilgnerFSDev <wilgner.fabio@gmail.com>
2025-04-02 01:07:48 -0300

    test: executes the command successfully when test_minimum_coverage is disabled + executes the command successfully when test_minimum_coverage is enabled

--- tests/Feature/Console/ShowBrainCommandTest.php
+++ tests/Feature/Console/ShowBrainCommandTest.php
@@ -7,7 +7,7 @@ use Brain\Facades\Terminal;
 use Illuminate\Console\OutputStyle;
 
 beforeEach(function (): void {
-    config()->set('brain.root', __DIR__.'/../Fixtures/Brain');
+    config()->set('brain.root', __DIR__ . '/../Fixtures/Brain');
     Terminal::shouldReceive('cols')->andReturn(71);
     $this->command = new ShowBrainCommand;
 });
@@ -20,9 +20,20 @@ it('has the correct description', function (): void {
     expect($this->command->getDescription())->toBe('Show Brain Mapping');
 });
 
-it('executes the command successfully', function (): void {
+it('executes the command successfully when test_minimum_coverage is disabled', function (): void {
+    config()->set('brain.test_minimum_coverage', 0.0);
     $mockOutput = Mockery::mock(OutputStyle::class);
-    $mockOutput->shouldReceive('writeln')->once();
+    $mockOutput->shouldReceive('writeln')->times(3);
+
+    $this->command->setOutput($mockOutput);
+
+    $this->command->handle();
+});
+
+it('executes the command successfully when test_minimum_coverage is enabled', function (): void {
+    config()->set('brain.test_minimum_coverage', 90.0);
+    $mockOutput = Mockery::mock(OutputStyle::class);
+    $mockOutput->shouldReceive('writeln')->times(4);
 
     $this->command->setOutput($mockOutput);
 
@@ -30,7 +41,7 @@ it('executes the command successfully', function (): void {
 });
 
 it('throws exception when brain is empty', function (): void {
-    config()->set('brain.root', __DIR__.'/../Fixtures/EmptyBrain');
+    config()->set('brain.root', __DIR__ . '/../Fixtures/EmptyBrain');
 
     $this->command->handle();
 })->throws(Exception::class, 'The brain map is empty.');

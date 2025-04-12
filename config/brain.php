38cb05e2128b68a9143b775a438d09a691f891a3
WilgnerFSDev <wilgner.fabio@gmail.com>
2025-04-11 21:17:26 -0300

    feat: added use_suffix option to config/brain.php

--- config/brain.php
+++ config/brain.php
@@ -14,4 +14,18 @@ return [
     |
     */
     'root' => app_path('Brain'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Suffix for Task, Process, and Query
+    |--------------------------------------------------------------------------
+    |
+    | When enabled (true), this setting appends a suffix to class names based
+    | on their type:
+    | - "Task" for tasks
+    | - "Process" for processes
+    | - "Query" for queries
+    |
+    */
+    'use_suffix' => true,
 ];
